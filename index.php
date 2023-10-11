1666ab3769e5aa44927500401185f5fb85c0d0f3
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-11 16:59:24 +0200

    add function generateRandomNumber

--- index.php
+++ index.php
@@ -16,6 +16,44 @@ Milestone 4 (BONUS)
 Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme). Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali.
  -->
 
+
+<?php
+
+var_dump($_GET);
+
+$inputPsw = $_GET['password'];
+
+function generateRandomPassword($length = 4)
+{
+    // Caratteri validi per la password
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_';
+
+    // Calcola la lunghezza della stringa dei caratteri
+    $characterLength = strlen($characters);
+
+    // Inizializza la password vuota
+    $password = '';
+
+    // Genera caratteri casuali fino a raggiungere la lunghezza desiderata
+    for ($i = 0; $i < $length; $i++) {
+        // Genera un numero casuale tra 0 e la lunghezza dei caratteri - 1
+        $randomIndex = random_int(0, $characterLength);
+
+        // Aggiungi il carattere corrispondente alla password
+        $password .= $characters[$randomIndex];
+    }
+
+    return $password;
+}
+
+// Generazione di una password casuale
+$password = generateRandomPassword();
+echo $password;
+
+var_dump($password)
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
