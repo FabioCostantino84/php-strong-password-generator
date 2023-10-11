ba9e0ced4220129e13685461cd0e1fcffa781246
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-11 23:47:15 +0200

    add fixed function

--- functions.php
+++ functions.php
@@ -1,50 +1,43 @@
 <?php
 
-var_dump($_GET);
-
 
 function generateRandomPassword() {
-
-    if (isset($_GET['password'])) {
-
-        $inputPsw = strlen($_GET['password']);
-
-        if ($inputPsw < 8) {
-            $error = 'inserisci almeno 8 caratteri';
-
-            return $error;
-            
-        } else {
-            $inputPsw = $_GET['password'];
-        
-            // Caratteri validi per la password
-    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_';
-
-    // Calcola la lunghezza della stringa dei caratteri
-    $characterLength = strlen($characters);
-
     // Inizializza la password vuota
     $password = '';
 
-    // Genera caratteri casuali fino a raggiungere la lunghezza desiderata
-    for ($i = 0; $i < strlen($inputPsw); $i++) {
-        // Genera un numero casuale tra 0 e la lunghezza dei caratteri
-        $randomIndex = random_int(0, $characterLength);
+    if (isset($_GET['password'])) {
+        $inputPsw = $_GET['password'];
 
-        // Aggiungi il carattere corrispondente alla password
-        $password .= $characters[$randomIndex];
+        if (strlen($inputPsw) < 8) {
+            $error = 'Inserisci almeno 8 caratteri';
+            return $error;
+        } else {
+
+            // Caratteri validi per la password
+            $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_';
+           
+            // Calcola la lunghezza della stringa dei caratteri
+            $characterLength = strlen($characters);
+
+            // Genera caratteri casuali fino a raggiungere la lunghezza desiderata
+            for ($i = 0; $i < strlen($inputPsw); $i++) {
+
+                // Genera un numero casuale tra 0 e la lunghezza dei caratteri - 1
+                $randomIndex = random_int(0, $characterLength - 1);
+
+                // Aggiunge il carattere corrispondente alla password
+                $password .= $characters[$randomIndex];
+            }
         }
-    }
-
+    } else {
+        $error = 'Password non specificata';
+        return $error;
     }
 
     return $password;
-};
+}
 
 // Generazione di una password casuale
 $password = generateRandomPassword();
-/* echo $password; */
-
-/* var_dump($password) */
 
 ?>
